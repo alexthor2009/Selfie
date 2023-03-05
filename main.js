7da9c992df99e03b6a3265f4b84f93d4ee899002
alexthor2009 <113790589+alexthor2009@users.noreply.github.com>
2023-03-05 11:43:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ recognition.onresult = function (event)
 {
     console.log(event);
 
-    var Content = event.results[0][0].tramscript;
+    var Content = event.results[0][0].transcript;
     console.log(Content);
 
     Textbox.innerHTML = Content;
@@ -39,6 +39,13 @@ recognition.onresult = function (event)
         synth.speak(utterThis);
 
         Webcam.attach(camera);
+
+        setTimeout(function()
+        {
+            take_selfie();
+
+            save();
+        }, 5000);
     }
 
     camera = document.getElementById("camera");
@@ -49,3 +56,18 @@ recognition.onresult = function (event)
         image_format:"png",
         png_quality:90
     });
+
+    function take_selfie()
+    {
+        Webcam.snap(function(data_uri)
+        {
+            document.getElementById("result").innerHTML='<img id = "selfie_image" src= "'+data_uri+'"/>';
+        });
+    }
+
+    function save(){
+        link=document.getElementById("link");
+        image=document.getElementById("selfie_image").src;
+        link.href=image
+        link.click();
+    }
